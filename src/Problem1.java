29302e606c127c48dae756e86264429d0b507af6
nek0722 <220439@astanait.edu.kz>
2023-03-30 18:26:23 +0600

    Implimentation of func which finds min value in arr

---
+++ src/Problem1.java
@@ -0,0 +1,22 @@
+import static java.lang.Integer.parseInt;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        int[] arr1 = {10, 1, 32, 3, 45};
+        int[] arr2 = {5, 4, -100, 49, 34, 54};
+        System.out.println("Minimum = " + FindMinimum(5, arr1));
+        System.out.println("Minimum = " + FindMinimum(arr2.length, arr2));
+
+    }
+    public static int FindMinimum(int size,int[] num){
+        int n=num[0];
+        for(int i=0;i<size;i++){
+            if(num[i]<n){
+                n=num[i];
+            }
+        }
+        return n;
+    }
+
+}
+
