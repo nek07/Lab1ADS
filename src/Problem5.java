62d79c99d476f0161426b974e6a059cf4a80988c
nek0722 <220439@astanait.edu.kz>
2023-03-30 19:51:38 +0600

    Implementation of func which find fibonnaci of nums(problem 5)

--- src/Problem5.java
+++ src/Problem5.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-public class Problem4 {
+public class Problem5 {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        System.out.println(factorial(scan.nextInt()));
+        System.out.println(fibonnaci(scan.nextInt()));
     }
-    public static int factorial(int n){
+    public static int fibonnaci(int n) {
         if(n<=1){
             return n;
         }
         else{
-            return factorial(n-1)*n;
+            return fibonnaci(n-1)+fibonnaci(n-2);
         }
     }
 }
