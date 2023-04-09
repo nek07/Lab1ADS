455fd1b2d48ef19fa60540555815c7f70f0e6ad2
nek0722 <220439@astanait.edu.kz>
2023-04-09 12:25:20 +0600

    commentaries

--- src/Lab1Main.java
+++ src/Lab1Main.java
@@ -13,105 +13,173 @@ public class Lab1Main {
 
 
     }
+    /*
+        @ FindMinimum - definition of min
+        @ size - length of array
+        @ num- array
+        @ return n - return minimum value
+
+     */
     public static int FindMinimum(int size,int[] num){
-        int n=num[0];
+        int n=num[0]; //store minimum element of array
         for(int i=0;i<size;i++){
-            if(num[i]<n){
+            if(num[i]<n){ //checks if element smaller or not
                 n=num[i];
             }
         }
-        return n;
+        return n; //return min
     }
 
+    /*
+        @ average - definition of average of arr elements
+        @ n - length of array
+        @ numbers - array
+        @ sum - contain sum of all elements
+        @ return - return average value
+
+     */
     public static double average(int n, double[] numbers){
         int sum=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){//sums all elements
             sum+=numbers[i];
         }
-        return (double)sum/n;
+        return (double)sum/n; //return the sum which divided by arr size
     }
+
+    /*
+        @ isPrime - definition of determining is it prime or composite
+        @ n - input number
+        @ divisor - local variable, helps to exclude composite nums.
+        @ return - return 'composite' or 'prime'
+
+     */
     public static String isPrime(int n){
-        if(n==1){
+        if(n==1){ //base case
             return "Composite";
         }
-        else if(n<=3){
+        else if(n<=3){ //base case
             return "Prime";
         }
-        else if(n%2==0 || n%3==0){
+        else if(n%2==0 || n%3==0){ //base case
             return "Composite";
         }
         int divisor=5;
-        while(divisor*divisor<=n){
-            if(n%divisor==0 || n%(divisor+2)==0){
+        while(divisor*divisor<=n){ //helps to check number
+            if(n%divisor==0 || n%(divisor+2)==0){ //exclude composite nums
                 return "Composite";
             }
             divisor+=6;
         }
-        return "Prime";
+        return "Prime"; //return final value
+    }
+    /*
+        @ factorial - definition of factorial of num
+        @ n - input number
+        @ return - return the factorial of number
+
+     */
+    public static int factorial(int n){
+        if(n<=1){ //base case
+            return n;
+        }
+        else{
+            return factorial(n-1)*n; //multiply n by (n-1) in descending order
+        }
     }
 
-    public static int factorial(int n){
-        if(n<=1){
+    /*
+        @ fibonacci - definition of fibonacci of num
+        @ n - input number
+        @ return - return the fibonacci
+
+     */
+    public static int fibonacci(int n) {
+        if(n<=1){ //base case
             return n;
         }
         else{
-            return factorial(n-1)*n;
+            return fibonacci(n-1)+fibonacci(n-2); //return the representation of formula which find fibonacci number
         }
     }
-    public static int fibonnaci(int n) {
-        if(n<=1){
-            return n;
-        }
-        else{
-            return fibonnaci(n-1)+fibonnaci(n-2);
-        }
-    }
+
+    /*
+        @ pow - definition of power of num
+        @ n - input number
+        @ return - return the pow of num
+
+     */
     public static int pow(int a,int n){
-        if(n==0){
+        if(n==0){ //base case, if n=0 according to formula a=1
             return 1;
         }
         else{
-            return pow(a,n-1)*a;
+            return pow(a,n-1)*a; //multiply a power times from input
         }
     }
+    /*
+        @ reverse - definition of reversing array
+        @ arr -  array which will be reversed
+        @ left - start index
+        @ right - stop index
+        @ return - return the reversed array
+
+     */
     public static int[] reverse(int[] arr,int left,int right){
-        if(right<=1){
+        if(right<=1){ //base case
             return arr;
         }
         else{
             int temp=arr[right];
             arr[right]=arr[left];
             arr[left]=temp;
-            return reverse(arr,left+1,right-1);
+            return reverse(arr,left+1,right-1); //replace first and last elements
         }
     }
+
+    /*
+        @ isDigit - definition of determining is string has only digits
+        @ str - contain char elements
+        @ Character.isDigit() - checks 1 char element is it digit or not
+        @ l - length of array
+    */
     public static boolean isDigit(char[] str, int l){
-        if(l==0){
+        if(l==0){//base case
             return true;
         }
         else{
             if(Character.isDigit(str[l])){
-                return isDigit(str,l-1);
+                return isDigit(str,l-1); //if it is digit the process continue
             }
             else{
-                return false;
+                return false; //if it is not digit func return false and stop
             }
         }
     }
-    public static int binomialCoefficent(int n,int k){
-        if(k==0 || k==n){
+
+    /*
+        @ binomialCoefficient - definition of binomail coef
+        @ n - first num according formula
+        @ k - second num according formula
+    */
+    public static int binomialCoefficient(int n,int k){
+        if(k==0 || k==n){ //base case according formula
             return 1;
         }
         else{
-            return binomialCoefficent(n-1,k-1)+binomialCoefficent(n-1,k);
+            return binomialCoefficient(n-1,k-1)+binomialCoefficient(n-1,k); //return the representation of binCoef formula
         }
     }
+    /*
+        @ greatCommonDivisor - definition of GCD
+        @ a - first num
+        @ b - second num
+    */
     public static int greatCommonDivisor(int a,int b){
-        if(b==0){
+        if(b==0){ //base case
             return a;
         }
         else{
-            return greatCommonDivisor(b,a%b);
+            return greatCommonDivisor(b,a%b); //return representation of Euclidean Algorithm
         }
     }
     public static void printArray(int[] arr){
